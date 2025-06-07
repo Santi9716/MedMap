f824b4bc4739daa1fee799de96c52b053b7b700b
Diego Garcia <juangarcia1708@hotmail.com>
2025-06-07 11:46:40 -0500

    ajustes previos a lugares

--- js/UsuarioRegistrado.js
+++ js/UsuarioRegistrado.js
@@ -4,7 +4,8 @@ export class UsuarioRegistrado extends Usuario {
     constructor(nombre, usuario, contraseña, email, celular) {
         super(nombre, usuario, contraseña, email, celular);
         this.lugaresVisitados = [];
-        this.lugaresPendientes = [];
+        this.lugaresPendientes = ["Plaza Botero", "Museo de Antioquia", "Palacio de la Cultura Rafael Uribe Uribe", "Comuna 13", "Pueblito Paisa", "Museo de Arte Moderno de Medellín", "Museo El Castillo", "Casa de la Memoria", "Cementerio Museo San Pedro", "Parque Arví", "Jardín Botánico de Medellín", "Parque Explora", "Planetario de Medellín", "Parque de los Pies Descalzos", "Parque de los Deseos", "Cerro Nutibara", "Cerro El Volador", "Parque Lineal La Presidenta", "Centro Comercial Santafé", "Tranvía de Ayacucho", "Metrocable", "Iglesia de la Veracruz", "Basílica de Nuestra Señora de la Candelaria", "Teatro Metropolitano", "Teatro Pablo Tobón Uribe"
+];
         this.fotos = [];
     }
 
